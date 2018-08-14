a1bc84c6c1ddcb16fa33e28838bc39bf2d6b3058
yshang0 <shanglue101@gmail.com>
2018-08-14 16:29:09 -0400

    accept and progress data:  thread used to deal with connections of clients;make the information from one client be shown on windows of all client;writeUTF, readUTF

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -3,20 +3,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
-
+// accept and progress data:  thread used to deal with connections of clients;
+//make the information from one client be shown on windows of all client;
+//writeUTF, readUTF
+//bug fix,
 public class ChatClient extends Frame {
     Socket s = null;
-    DataOutputStream dos;
+    DataOutputStream dos = null;
+    DataInputStream dis = null;
+
+    private boolean bConnected = false;
+
     TextField tfTxt = new TextField();
     //A TextField object is a text component that allows for the editing of a single line of text.
 
     TextArea taContent = new TextArea();
     //A TextArea object is a multi-line region that displays text. It can be set to allow editing or to be read-only.
+
+    Thread tRecv = new Thread(new RecvThread());
 
     public static void main(String[] args) {
         ChatClient cc = new ChatClient();
@@ -35,6 +47,7 @@ public class ChatClient extends Frame {
 
         this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent arg0){
+                disconnect();
                 System.exit(0);
             }
         });
@@ -46,13 +59,16 @@ public class ChatClient extends Frame {
 
         setVisible(true);
         connect();
+        tRecv.start();
     }
 
     public void connect() {
         try {
-            s = new Socket("127.0.0.1", 8888);
+            s = new Socket("127.0.0.1", 8888);int[] v = new int[26];
             dos = new DataOutputStream(s.getOutputStream());
+            dis = new DataInputStream(s.getInputStream());
             System.out.println("connected!");
+            bConnected = true;
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -61,13 +77,36 @@ public class ChatClient extends Frame {
     }
 
     public void disconnect() {
-
         try {
             dos.close();
+            dis.close();
             s.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+
+
+
+        /*
+        try {
+            bConnected = false;
+
+            tRecv.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }  finally {
+            try {
+                dos.close();
+                dis.close();
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }*/
     }
 
 
@@ -89,4 +128,25 @@ public class ChatClient extends Frame {
         }
     }
     //ActionListener: The listener interface for receiving action events.
+
+    private class RecvThread implements Runnable {
+
+        public void run() {
+
+                try {
+                    while(bConnected) {
+                        String str = dis.readUTF(); // stuck here;
+                        //System.out.println(str);
+                        taContent.setText(taContent.getText() + str + '\n');
+
+                    }
+                } catch (SocketException e) {
+                    System.out.println("exit, bye");
+                } catch (EOFException e) {
+                    System.out.println("exit");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -2,18 +2,94 @@ import java.io.*;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.*;
 
 public class ChatServer {
+    boolean started = false;
 
+    ServerSocket ss = null;
 
+    static List<Client> clients = new ArrayList<Client>();
 
     public static void main(String[] args) {
-        boolean started = false;
-        Socket s = null;
-        ServerSocket ss = null;
-        DataInputStream dis = null;
+
+        new ChatServer().start();
+        //based on TCP, and start a server, and accept the connection from client;
+    }
+
+    static class Client implements Runnable { // represent the package of client on server;
+        private Socket s; // represent the connection between the
+        private DataInputStream dis = null;
+        private DataOutputStream dos = null;
+        private boolean bConnected = false;
+
+
+
+        public Client(Socket s) {
+            this.s = s;
+            try {
+                dis = new DataInputStream(s.getInputStream());
+                dos = new DataOutputStream(s.getOutputStream());
+                bConnected = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void send(String str) throws IOException {
+            try {
+                dos.writeUTF(str);
+            } catch (IOException e) {
+                clients.remove(this);
+                System.out.println("the opposite exit, and I exclude it");
+                //e.printStackTrace();
+            }
+
+        }
+
+        public void run() {
+            try {
+                while (bConnected) {
+                    String str = dis.readUTF();
+                    System.out.println(str);
+                    for (int i = 0; i < clients.size(); i++) {
+                        Client c = clients.get(i);
+                        c.send(str);
+                        //System.out.println(" a string send !");
+                    }
+                }
+
+            } catch (EOFException e) {
+                    System.out.println("Client disconnected");
+            } catch (IOException e) {
+                    e.printStackTrace();
+            } finally {
+                    try {
+                        if(dis!= null) dis.close();
+                        if(dos!= null) dos.close();
+                        if(s != null) {
+                            s.close();
+                            //s = null; // it will be recycled;
+                        }
+
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+
+                    if(this != null) {
+                        clients.remove(this);
+                    }
+            }
+
+        }
+
+    }
+
+    public void start() {
         try {
             ss = new ServerSocket(8888);//already in use;
+            started = true;
         } catch(BindException e) {
             System.out.println("port already in use.....");
             System.out.println("close and restart");
@@ -24,35 +100,29 @@ public class ChatServer {
 
 
         try{
-            started = true;
+
+            // use one Thread to control one client;
+            // main Thread only control the connection of client;
+            // other Thread used to communication of server;
             while(started) {
-                boolean bConnected = false;
 
-                s = ss.accept(); // accept the connection from client;
+                Socket s = ss.accept(); // accept the connection from client;
+
+                Client c = new Client(s);
                 System.out.println("a client connected");
-                bConnected = true;
-
-                dis = new DataInputStream(s.getInputStream());
-                while(bConnected) {
-                    String str = dis.readUTF();
-                    System.out.println(str);
-                }
+                new Thread(c).start();
+                clients.add(c);
                 //dis.close();
             }
-        } catch (EOFException e) {
-            System.out.println("Client connected");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                if(dis!= null) dis.close();
-                if(s!= null) s.close();
-            } catch (IOException e1) {
-                e1.printStackTrace();
+                ss.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-
-
-        //based on TCP, and start a server, and accept the connection from client;
     }
+
 }
